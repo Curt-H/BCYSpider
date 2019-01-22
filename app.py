0a9ff00eb7e3204ee36cc08b1e0176ed17315541
Curt the Guardian <guardian.curt@outlook.com>
2019-01-22 17:07:17 +0800

    Update app.py
    Try to use post to login account and not succeed

--- app.py
+++ app.py
@@ -2,8 +2,18 @@
 
 from utils import log
 from modules import page
+from requests import *
 
 if __name__ == '__main__':
     log('Start')
-    r = page.get_content('https://bcy.net')
-    log(r)
+    # r = page.get_content('https://bcy.net')
+    # log(r)
+    # https://bcy.net/passport/web/user/login/?account_sdk_source=web
+    data = dict(
+        account='15695618017',
+        password='FFF000123'
+    )
+    log(data)
+
+    r = post('https://bcy.net/passport/web/user/login/?account_sdk_source=web', data=data)
+    log(r.text)
