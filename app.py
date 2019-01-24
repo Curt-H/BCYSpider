8be993d8f3327a4e0f4d7ec8d3cbea923d15b5ab
Curt the Guardian <guardian.curt@outlook.com>
2019-01-24 11:49:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from utils import log
 from modules import cache, login
+from modules.following import get_following_list
 from requests import *
 
 if __name__ == '__main__':
@@ -10,3 +11,4 @@ if __name__ == '__main__':
     # log(r)
     # https://bcy.net/passport/web/user/login/?account_sdk_source=web
     login.log_in()
+    get_following_list()
