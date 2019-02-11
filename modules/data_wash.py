de0e5a654c62cefb487656b597e5d11afcc04486
Curt the Guardian <guardian.curt@outlook.com>
2019-02-11 09:53:42 +0800

    Update data_wash.py

---
+++ modules/data_wash.py
@@ -0,0 +1,11 @@
+from pyquery import PyQuery as pq
+from modules.cache import get_content
+from modules import generate_coser_url
+
+
+def get_posts_list(coser_id):
+    c = coser_id
+    url = generate_coser_url(c)
+    content = get_content(url)
+
+    
