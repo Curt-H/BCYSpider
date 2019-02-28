30e6653bf2dcb53a1949c6110bfbc015663da049
Curt the Guardian <guardian.curt@outlook.com>
2019-02-28 13:46:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     # r = page.get_content('https://bcy.net')
     # log(r)
     # https://bcy.net/passport/web/user/login/?account_sdk_source=web
-    login.log_in()
-    get_following_list()
+    # login.log_in()
+    # get_following_list()
     get_posts_list('446923')
